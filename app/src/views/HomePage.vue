6887f1a384d85653e34a79f5287063f4b3a83309
Chris Greenwood <chrisg@just.property>
2023-07-24 07:01:02 +0200

    Init commit

---
+++ app/src/views/HomePage.vue
@@ -0,0 +1,64 @@
+<template>
+  <ion-header :translucent="true">
+    <ion-toolbar>
+      <!-- <ion-buttons slot="start">
+        <ion-back-button default-href="#"></ion-back-button>
+      </ion-buttons> -->
+      <ion-title>{{ page }}</ion-title>
+      <ion-buttons slot="end">
+        <ion-button>
+          <ion-icon slot="icon-only" :icon="search"></ion-icon>
+        </ion-button>
+      </ion-buttons>
+
+      <ion-buttons slot="end">
+        <ion-button>
+          <ion-icon  slot="icon-only" :icon="notifications"></ion-icon>
+        </ion-button>
+      </ion-buttons>
+
+
+      <ion-buttons slot="end">
+        <ion-menu-button :auto-hide="false"></ion-menu-button>
+      </ion-buttons>
+    </ion-toolbar>
+  </ion-header>
+</template>
+
+<script lang="ts">
+import { IonBackButton, IonButton, IonButtons, IonIcon, IonMenuButton, IonTitle, IonToolbar, IonFooter, IonHeader } from '@ionic/vue';
+import { create, ellipsisHorizontal, ellipsisVertical, helpCircle, search, personCircle, star, notifications} from 'ionicons/icons';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  components: {
+    IonBackButton,
+    IonButton,
+    IonButtons,
+    IonIcon,
+    IonMenuButton,
+    IonTitle,
+    IonToolbar,
+    IonFooter,
+    IonHeader
+  },
+  setup() {
+    return {
+      create,
+      ellipsisHorizontal,
+      ellipsisVertical,
+      helpCircle,
+      search,
+      personCircle,
+      star
+    };
+  },
+  data() {
+    return {
+      page: "Leads"
+    }
+  }
+});
+</script>
+
+<style scoped></style>
